4047568ce71670e765dd4206f6ee953d777b6aa8
LeVanChuyen-20 <20222411@eaut.edu.vn>
2025-04-10 09:07:17 +0700

    creat

---
+++ src/main/java/com/livestockmanagementapi/LivestockManagementApiApplication.java
@@ -0,0 +1,13 @@
+package com.livestockmanagementapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LivestockManagementApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LivestockManagementApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/livestockmanagementapi/LivestockManagementApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.livestockmanagementapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LivestockManagementApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
